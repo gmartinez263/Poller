b55cadb75c54d845e6ea92ec9c7a186d12afb838
Geovanny <gmartinez@techexchange.in>
2023-02-08 07:41:21 +0000

    HW1 solution

--- poller.py
+++ poller.py
@@ -1,58 +1,103 @@
 class Participant:
     def __init__(self, name, polled, correct, attempted, excused):
-         self.name = name
-         self.polled = polled
-         self.correct = correct
-         self.attempted = attempted
-         self.excused = excused
+        self.name = name
+        self.polled = polled
+        self.correct = correct
+        self.attempted = attempted
+        self.excused = excused
 
-    def toString(Self):
-         return self.name + "," + self.polled + "," + self.correct + "," + self.attempted + "," + self.excused
+    def __str__(self):
+        return self.name + "," + str(self.polled) + "," + str(self.correct) + "," + str(self.attempted) + "," + str(self.excused)
 
+import random
 class Poller:
-    import random
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.num_people_polled = 0
 
-     def __init__(self, file_name):
-         self.file_name = file_name
-         self.participant_list = self.createParticipants(self.file_name)
-         self.current_participant = None
+    def __enter__(self):
+        self.file = open(self.file_name, "r+")
 
-     def createParticipants(self, file_name):
-         file = open(file_name, "r")
-         participant_list = []
+        for person in self.file:
+            person_size = person.split(",")
 
-         for person in file:
-             poll_data = person.split(',')
-             name = poll_data[0]
-             polled = poll_data[1]
-             correct = poll_data[2]
-             attempted = poll_data[3]
-             excused = poll_data[4]
-             participant_list.append(Participant(name, int(polled), int(correct), int(attempted), int(excused)))
+            if(len(person_size) < 5 or len(person_size) > 5):
+                raise ValueError
+            elif(not self.has_correct_attributes(person_size)):
+                raise ValueError
 
-         file.close()
-         return participant_list
+        self.file.seek(0)
+        return self
 
-     def correct(self):
-         self.current_participant.correct += 1
-         self.current_participant.polled += 1
+    def has_correct_attributes(self, attributes):
+        correct_attributes = True
+        passed = True
+        for i in range(len(attributes) - 1):
+            if(len(attributes[i + 1]) > 1):
+                tot = 0
+                j = 0
+      
+                for char in attributes[i + 1]:
+                    tot += ord(char)
+                    j+=1
+
+                if(tot > j * 57):
+                    passed = False
         
-     def __exit__(self):
-         file = open(self.file_name, "w") 
+            elif(ord(attributes[i + 1]) < 48 or ord(attributes[i + 1]) > 57):
+                correct_attributes = False
+    
+        return (correct_attributes and passed)
 
-         for participant in self.participant_list:
-             file.write(participant.toString() + "\n")
+    def total(self):
+        return self.num_people_polled
+
+    def __iter__(self):
+        self.participants = self.create_participants()
+        return self
+
+    def __next__(self):
+        rand_participant = random.randint(0, len(self.participants) - 1)
+        self.current_participant = self.participants[rand_participant]
+        self.num_people_polled += 1
+        return self.current_participant
+
+    def stop(self):
+        raise StopIteration
+
+    def create_participants(self):
+        participants = []
+
+        for person in self.file:
+            poll_data = person.split(',')
+            name = poll_data[0]
+            polled = poll_data[1]
+            correct = poll_data[2]
+            attempted = poll_data[3]
+            excused = poll_data[4]
+            participants.append(Participant(name, int(polled), int(correct), int(attempted), int(excused)))
+        self.file.close()
+        return participants
+
+    def correct(self):
+        self.current_participant.correct += 1
+        self.current_participant.polled += 1
         
-         file.close()
+    def attempted(self):
+        self.current_participant.attempted += 1
+        self.current_participant.polled += 1
 
-     def __iter__(self):
-         return self
+    def excused(self):
+        self.current_participant.excused += 1
+        self.current_participant.polled += 1
 
-     def __next__(self):
-         
-         
-        def __enter__(self):
-            
+    def missing(self):
+        self.current_participant.polled += 1
         
-     def attempted():
-         pass
+    def __exit__(self, type, value, traceback):
+        self.file = open(self.file_name, "w")
+        for participant in self.participants:
+            self.file.write(str(participant) + "\n")
+        self.file.close()
+        
+       
--- poller_test.py
+++ poller_test.py
@@ -1,7 +1,17 @@
 import pytest
 from poller import Participant
+from poller import Poller
+from unittest import mock
 
 
 def test_participant():
     participant = Participant("Isabelle",5,3,1,1)
     assert str(participant) == "Isabelle,5,3,1,1"
+
+@mock.patch('builtins.open')
+def test_dunder(mock_open):
+    mock_open.return_value = mock.Mock()
+    mock_open.return_value.read.return_value = "Isabelle Watanabe,0,0,0,0"
+    with Poller("") as poller:
+        poller.attempted()
+        assert str(poller) == "Isabelle Watanabe,1,0,1,0"      
